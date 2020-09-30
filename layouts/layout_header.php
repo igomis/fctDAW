34ee16c04e0364a659c90ed0b82139f88a06c3bc
igomis <igomis@cipfpbatoi.es>
2020-09-30 20:51:19 +0200

    R21

---
+++ layouts/layout_header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php echo $page_title; ?></title>
+
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- our custom CSS -->
+    <link rel="stylesheet" href="/libs/css/custom.css" />
+    <!-- fonts -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+
+
+</head>
+<body>
+
+<!-- container -->
+<div class="container">
+
+    <div class='pb-2 mt-4 mb-2 border-bottom'>
+        <h1><?= $page_title ?></h1>
+    </div>
+
