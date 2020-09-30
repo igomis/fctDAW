5388c6b02b8e87a48392484e66b85c8138664a53
igomis <igomis@cipfpbatoi.es>
2020-09-30 23:07:24 +0200

    R23a

---
+++ objects/enterprise.php
@@ -0,0 +1,70 @@
+<?php
+class Enterprise
+{
+    // database connection and table name
+    private $conn;
+    private $table_name = "enterprises";
+
+    // object properties
+    public $cif;
+    public $name;
+    public $activity;
+    public $adress;
+    public $location;
+    public $email;
+    public $phone;
+    public $places = 1;
+    public $comments;
+
+
+    /**
+     * Enterprise constructor.
+     * @param Object $db
+     */
+    public function __construct(Object $db){
+        $this->conn = $db;
+    }
+
+
+    /**
+     * @return bool
+     */
+    function create(){
+
+
+        $query = "INSERT INTO " . $this->table_name . " SET
+                    cif=:cif, name=:name, activity=:activity, adress=:adress, location=:location ,
+                    email=:email, phone=:phone, places=:places, comments=:comments";
+
+        $stmt = $this->conn->prepare($query);
+
+        // posted values
+        $this->cif=htmlspecialchars(strip_tags($this->cif));
+        $this->name=htmlspecialchars(strip_tags($this->name));
+        $this->activity=htmlspecialchars(strip_tags($this->activity));
+        $this->adress=htmlspecialchars(strip_tags($this->adress));
+        $this->location=htmlspecialchars(strip_tags($this->location));
+        $this->email=htmlspecialchars(strip_tags($this->email));
+        $this->phone=htmlspecialchars(strip_tags($this->phone));
+        $this->places=htmlspecialchars(strip_tags($this->places));
+        $this->comments=htmlspecialchars(strip_tags($this->comments));
+
+
+
+        // bind values
+        $stmt->bindParam(":cif", $this->cif);
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":activity", $this->activity);
+        $stmt->bindParam(":adress", $this->adress);
+        $stmt->bindParam(":location", $this->location);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":phone", $this->phone);
+        $stmt->bindParam(":places", $this->places);
+        $stmt->bindParam(":comments", $this->comments);
+
+
+        if($stmt->execute()) return true;
+
+        return false;
+    }
+}
---
+++ public/create_page.php
@@ -0,0 +1,110 @@
+<?php
+// include database and object files
+include_once '../config/config.php';
+include_once '../config/database.php';
+include_once '../objects/Enterprise.php';
+
+
+// get database connection
+
+$database = new Database();
+$db = $database->getConnection();
+$enterprise = new Enterprise($db);
+
+$page_title = "Crear Empresa";
+include_once "../layouts/layout_header.php";
+?>
+// contents will be here
+<div class='right-button-margin'>
+    <a href='index.php' class='btn btn-outline-primary float-right'>Vore Empreses</a>
+</div>
+<!-- PHP post code will be here -->
+<?php
+
+if($_POST){
+
+
+    // set product property values
+    $enterprise->cif = $_POST['cif'];
+    $enterprise->name = $_POST['name'];
+    $enterprise->activity = $_POST['activity'];
+    $enterprise->adress = $_POST['adress'];
+    $enterprise->location = $_POST['location'];
+    $enterprise->email = $_POST['email'];
+    $enterprise->phone = $_POST['phone'];
+    $enterprise->places = $_POST['places'];
+    $enterprise->comments = $_POST['comments'];
+
+
+    // create the product
+    if($enterprise->create()){
+        echo "<div class='alert alert-success'>L'Empresa ha estat creada.</div>";
+    }
+
+    // if unable to create the product, tell the user
+    else{
+        echo "<div class='alert alert-danger'>No he pogut crear l'empresa.</div>";
+    }
+}
+?>
+
+<!-- HTML form for creating a product -->
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+
+    <div class="form-row">
+        <div class="form-group col-md-2">
+            <label for="cif">CIF</label>
+            <input type='text' name='cif' class='form-control' id="cif" />
+        </div>
+        <div class="form-group col-md-4">
+            <label for="name">Nom</label>
+            <input type='text' name='name' class='form-control' id="name" />
+        </div>
+        <div class="form-group col-md-6">
+            <label for="activity">Activitat</label>
+            <input type='text' name='activity' class='form-control' id="activity" />
+        </div>
+    </div>
+
+    <div class="form-row">
+        <div class="form-group col-md-8">
+            <label for="adress">Adreça</label>
+            <input type='text' name='adress' class='form-control' id="adress" />
+        </div>
+        <div class="form-group col-md-4">
+            <label for="location">Localitat</label>
+            <input type='text' name='location' class='form-control' id="location" />
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="form-group col-md-5">
+            <label for="contact">Persona de contacte</label>
+            <input type='text' name='contact' class='form-control' id="contact" />
+        </div>
+        <div class="form-group col-md-3">
+            <label for="email">Email</label>
+            <input type='email' name='email' class='form-control' id="email" />
+        </div>
+        <div class="form-group col-md-2">
+            <label for="phone">Telèfon</label>
+            <input type='tel' name='phone' class='form-control' id="phone" />
+        </div>
+        <div class="form-group col-md-2">
+            <label for="places">Nombre de places</label>
+            <input type='number' name='places' class='form-control' id="places" />
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label for="comments">Comentaris</label>
+        <textarea name='comments' class='form-control' id="coments" ></textarea>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Crear</button>
+</form>
+
+<?php
+// footer
+include_once "../layouts/layout_footer.php";
+?>
+
